4d6caa33bae4d6bb0e9c40566702a1e5811b1dc7
alfian <alfianmusthofa45264@gmail.com>
2023-09-12 05:42:58 +0700

    alfian

--- TicketID/script/index.js
+++ TicketID/script/index.js
@@ -42,79 +42,88 @@ document.addEventListener('click', function(e){
     }
 })
 
-// $('.menu-button-hotel1').on('click', function() {
-//     $('.menu-button-hotel1').removeClass('active')
-//     $(this).addClass('active');
 
-//     $('.menu-button-hotel1').css({
-//         'backgroundColor':'',
-//         'border':''
-//     });
-//     $('.menu-button-hotel1.active').css({
-//         'backgroundColor':'#cce6ff',
-//         'border':'1px solid blue'
-//     });
+$(document).ready(function() {
+    for (let i = 1; i <= 3; i++) { 
+        $('.menu-button-hotel' + i).on('click', function() {
+            $('.menu-button-hotel' + i).removeClass('active');
+            $(this).addClass('active');
 
-//     let cityName = $(this).html();
+            let cityName = $(this).html();
 
-//     $.getJSON('JSON/data-tempat1.json', function(data) {
-//         let content = '';
-//         $.each(data, function(i, data) {
-//             if(data.city == cityName){
-//                 content += '<div class="card"><a href="#"><div class="card-images"><img src="image/tempat1/'+ data.image +'"></div><div class="caption"><h3 class="hotel-name">'+data.name+'</h3><div class="starate">'+data.rating+'<p class="address">'+data.address+'</p></div><div class="review-caps">'+data.review+'</div><div class="price"><h3 class="price">'+data.price+'</h3></div></div></a></div>'
-//             }
-//         });
-        
-//         $('#daftar-tempat1').html(content);
-//     });
-// });
-
-
-
-
-// MAINTAINCE STYLE
-// $(document).ready(function() {
-//     for (let i = 1; i <= 3; i++) { 
-//         $('.menu-button-hotel' + i).on('click', function() {
-//             $('.menu-button-hotel' + i).removeClass('active');
-//             $(this).addClass('active');
-
-//             let cityName = $(this).html();
-
-//             $.getJSON('JSON/data-tempat' + i + '.json', function(data) {
-//                 let content = '';
-//                 $.each(data, function(j, data) {
-//                     if (data.city == cityName) {
-//                         content += '<div class="card"><a href="#"><div class="card-images"><img src="image/tempat' + i + '/' + data.image + '"></div><div class="caption"><h3 class="hotel-name">' + data.name + '</h3><div class="starate">' + data.rating + '<p class="address">' + data.address + '</p></div><div class="review-caps">' + data.review + '</div><div class="price"><h3 class="price">' + data.price + '</h3></div></div></a></div>'
-//                     }
-//                 });
+            $.getJSON('JSON/data-tempat' + i + '.json', function(data) {
+                let content = '';
+                $.each(data, function(j, data) {
+                    if (data.city == cityName) {
+                        content += `<a href="#">
+                        <div class="card">
+                            <img src="image/tempat${i}/${data.image}">
+                            <div class="caption">
+                                <div class="hotel-name">
+                                    <h3>${data.name}</h3>
+                                </div>
+                                <div class="rating">
+                                    <div class="star">
+                                        ${data.rating}
+                                    </div>
+                                    <p>${data.address}</p>
+                                </div>
+                                <div class="review">
+                                    <div class="logo">
+                                        <i class="fa-solid fa-award"></i>
+                                    </div>
+                                    <p>${data.review}</p>
+                                </div>
+                                <div class="price">
+                                    <p>${data["last-price"]}</p>
+                                    <h3>${data.price}</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </a>`
+                    }
+                });
                 
-//                 $('#daftar-tempat' + i).html(content);
-//             });
-//         });
-//     }
-// });
-
-
-// MENDAPATKAN DATA DARI JSON
-
-// $.getJSON('JSON/data-tempat.json', function(data) {
-//     $.each(data, function(i, data) {
-//         $('#daftar-tempat1').append('<div class="card"><a href="#"><div class="card-images"><img src="image/hotel/'+ data.image +'"></div><div class="caption"><h3 class="hotel-name">'+data.name+'</h3><div class="starate">'+data.rating+'<p class="address">'+data.address+'</p></div><div class="review-caps">'+data.review+'</div><div class="price"><h3 class="price">'+data.price+'</h3></div></div></a></div>');
-//     })
-// });
+                $('#daftar-tempat' + i).html(content);
+            });
+        });
+    }
+});
 
 
 
-// MAINTAINCE STYLE
-// const fileNumbers = [1, 2, 3];
-
-// $(document).ready(function() {
-//     fileNumbers.forEach(function(number) {
-//         $.getJSON('JSON/data-tempat' + number + '.json', function(data) {
-//             $.each(data, function(i, data) {
-//                 $('#daftar-tempat'+ number).append('<div class="card"><a href="#"><div class="card-images"><img src="image/tempat' + number + '/' + data.image + '"></div><div class="caption"><h3 class="hotel-name">'+data.name+'</h3><div class="starate">'+data.rating+'<p class="address">'+data.address+'</p></div><div class="review-caps">'+data.review+'</div><div class="price"><h3 class="price">'+data.price+'</h3></div></div></a></div>');
-//             });
-//         });
-//     });
-// });
+const fileNumbers = [1, 2, 3];
+$(document).ready(function() {
+    fileNumbers.forEach(function(number) {
+        $.getJSON('JSON/data-tempat' + number + '.json', function(data) {
+            $.each(data, function(i, data) {
+                $('#daftar-tempat'+ number).append(`<a href="#">
+                <div class="card">
+                    <img src="image/tempat${number}/${data.image}">
+                    <div class="caption">
+                        <div class="hotel-name">
+                            <h3>${data.name}</h3>
+                        </div>
+                        <div class="rating">
+                            <div class="star">
+                                ${data.rating}
+                            </div>
+                            <p>${data.address}</p>
+                        </div>
+                        <div class="review">
+                            <div class="logo">
+                                <i class="fa-solid fa-award"></i>
+                            </div>
+                            <p>${data.review}</p>
+                        </div>
+                        <div class="price">
+                            <p>${data["last-price"]}</p>
+                            <h3>${data.price}</h3>
+                        </div>
+                    </div>
+                </div>
+            </a>`);
+            });
+        });
+    });
+});
